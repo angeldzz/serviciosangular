2ecb356b8345b9c01eacb8543a6daf761f1e6c8d
angeldzz <pintodiazangel@gmail.com>
2025-11-07 11:53:46 +0100

    hay que importar components en standalone

--- src/app/components/personasstandalone.component/personasstandalone.component.ts
+++ src/app/components/personasstandalone.component/personasstandalone.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Persona } from '../../models/persona';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-personasstandalone',
   standalone: true,
   templateUrl: './personasstandalone.component.html',
   styleUrl: './personasstandalone.component.css',
+  imports: [CommonModule]
 })
 export class PersonasstandaloneComponent {
-
+  public personas!: Array<Persona>;
+  constructor() {
+    this.personas = new Array<Persona>();
+    let p = new Persona(1,"A","B",22);
+    let p2 = new Persona(2,"C","D",22123);
+    this.personas.push(p,p2)
+  }
 }
